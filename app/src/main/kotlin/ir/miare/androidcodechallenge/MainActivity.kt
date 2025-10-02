2c6781ba9743d0e07c4eb37b9a992efefdb38b98
javadjafari <javad2147@yahoo.com>
2025-10-02 17:25:53 +0330

    add Material3Theme

--- app/src/main/kotlin/ir/miare/androidcodechallenge/MainActivity.kt
+++ app/src/main/kotlin/ir/miare/androidcodechallenge/MainActivity.kt
@@ -3,14 +3,27 @@ package ir.miare.androidcodechallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import ir.miare.androidcodechallenge.theme.AppTheme
 
-class MainActivity : ComponentActivity() {
+internal class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            Text("Hello Compose")
+            AppTheme {
+                Scaffold {
+                    Column(Modifier.padding(it)) {
+                        Text("Hello Compose")
+                    }
+                }
+            }
         }
     }
 }
---
+++ app/src/main/kotlin/ir/miare/androidcodechallenge/theme/Type.kt
@@ -0,0 +1,5 @@
+package ir.miare.androidcodechallenge.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
