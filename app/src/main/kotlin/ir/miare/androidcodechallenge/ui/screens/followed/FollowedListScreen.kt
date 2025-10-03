836181a86108187ae7a2508198b822bd1f81c349
javadjafari <javad2147@yahoo.com>
2025-10-04 00:29:48 +0330

    implement the Empty state of the FollowedListScreen.kt

--- app/src/main/kotlin/ir/miare/androidcodechallenge/ui/screens/followed/FollowedListScreen.kt
+++ app/src/main/kotlin/ir/miare/androidcodechallenge/ui/screens/followed/FollowedListScreen.kt
@@ -1,17 +1,23 @@
 package ir.miare.androidcodechallenge.ui.screens.followed
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
@@ -19,10 +25,14 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -123,30 +133,59 @@ internal fun FollowedListScreen(
             )
         }
     ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize(),
-            contentPadding = PaddingValues(
-                vertical = 8.dp,
-            )
-        ) {
-            items(
-                players,
-                key = { it.id }
-            ) { player ->
-                PlayerItemContent(
-                    modifier = Modifier.animateItem(
-                        fadeInSpec = tween(durationMillis = 250),
-                        fadeOutSpec = tween(durationMillis = 100),
-                        placementSpec = spring(
-                            stiffness = Spring.StiffnessLow,
-                            dampingRatio = Spring.DampingRatioLowBouncy
+        val shouldShowEmptyState by remember(players) {
+            derivedStateOf { players.isEmpty() }
+        }
+        AnimatedContent(shouldShowEmptyState) { isEmpty ->
+            if (isEmpty) {
+                Box(Modifier.fillMaxSize()) {
+                    Column(
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(46.dp),
+                            painter = painterResource(R.drawable.ic_person),
+                            contentDescription = null,
                         )
-                    ),
-                    player = player,
-                    onClick = { onPlayerClicked(player.id) }
-                )
+
+                        Text(
+                            text = stringResource(R.string.your_followed_list_is_empty),
+                            style = MaterialTheme.typography.headlineSmall,
+                            color = MaterialTheme.colorScheme.onBackground,
+                        )
+                    }
+                }
+            } else {
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .fillMaxSize(),
+                    contentPadding = PaddingValues(
+                        vertical = 8.dp,
+                    )
+                ) {
+                    items(
+                        players,
+                        key = { it.id }
+                    ) { player ->
+                        PlayerItemContent(
+                            modifier = Modifier.animateItem(
+                                fadeInSpec = tween(durationMillis = 250),
+                                fadeOutSpec = tween(durationMillis = 100),
+                                placementSpec = spring(
+                                    stiffness = Spring.StiffnessLow,
+                                    dampingRatio = Spring.DampingRatioLowBouncy
+                                )
+                            ),
+                            player = player,
+                            onClick = { onPlayerClicked(player.id) }
+                        )
+                    }
+                }
             }
         }
     }
